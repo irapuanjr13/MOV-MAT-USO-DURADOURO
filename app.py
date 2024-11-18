4c273975aa64023739f4ad0e49f878f376c5040f
irapuanjr13 <irapuanjunior13@gmail.com>
2024-11-18 17:29:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -153,5 +153,5 @@ def get_chefia():
      return jsonify({"chefia": chefia[0] if len(chefia) > 0 else ""})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))  # Usa a porta do ambiente ou 5001 como fallback
+    port = int(os.environ.get("PORT", 5002))  # Usa a porta do ambiente ou 5002 como fallback
     app.run(debug=True, port=port)
