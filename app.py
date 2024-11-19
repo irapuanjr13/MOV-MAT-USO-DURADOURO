6d7196b0622acbb621c773cb59036a8aa1a9a934
irapuanjr13 <irapuanjunior13@gmail.com>
2024-11-19 18:54:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -196,3 +196,6 @@ def get_chefia():
         return jsonify({"error": "Tipo inválido!"}), 400
 
     return jsonify({"chefia": chefia.tolist()})
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))  # Lê a variável PORT ou usa 5000 como padrão
+    app.run(host="0.0.0.0", port=port)
